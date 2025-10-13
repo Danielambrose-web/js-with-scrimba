bfc64563b701859d297a6013ebee3de9c50e1c12
Danielambrose-web <your-email@example.com>
2025-10-13 22:28:54 +0100

    challengr // Create a function that increments the lapsCompleted variable with one // Run it three times

--- 1/index.js
+++ 1/index.js
@@ -14,3 +14,18 @@
 
 // console.log(myPoints)
 
+// Create a function that increments the lapsCompleted variable with one
+// Run it three times
+
+let lapsCompleted = 0
+
+
+function runTime(){
+    lapsCompleted ++
+}
+runTime()
+runTime()
+runTime()
+
+console.log(lapsCompleted)
+
