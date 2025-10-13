31f2b792b51ed964946c1051078c3398f15fd5d9
Danielambrose-web <your-email@example.com>
2025-10-13 20:41:20 +0100

    Challenge // Create two functions, add3Points() and remove1Point(), and have them // add/remove points to/from the myPoints variable

---
+++ 1/index.js
@@ -0,0 +1,18 @@
+let myPoints = 3
+
+// Create two functions, add3Points() and remove1Point(), and have them
+// add/remove points to/from the myPoints variable
+
+function add3Points(){
+    myPoints += 3
+}
+function sub(){
+    myPoints -= 1
+}
+add3Points()
+add3Points()
+add3Points()
+sub()
+sub()
+
+console.log(myPoints)
