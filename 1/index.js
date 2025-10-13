a7457598124ac730053b1fef8ae72409f646f36b
Danielambrose-web <your-email@example.com>
2025-10-13 20:55:28 +0100

    no much

--- 1/index.js
+++ 1/index.js
@@ -1,18 +1,16 @@
-let myPoints = 3
+//let myPoints = 3
 
-// Create two functions, add3Points() and remove1Point(), and have them
-// add/remove points to/from the myPoints variable
+// function add3Points(){
+//     myPoints += 3
+// }
+// function sub(){
+//     myPoints -= 1
+// }
+// add3Points()
+// add3Points()
+// add3Points()
+// sub()
+// sub()
 
-function add3Points(){
-    myPoints += 3
-}
-function sub(){
-    myPoints -= 1
-}
-add3Points()
-add3Points()
-add3Points()
-sub()
-sub()
+// console.log(myPoints)
 
-console.log(myPoints)
